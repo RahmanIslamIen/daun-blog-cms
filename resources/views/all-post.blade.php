08c62e4c27bc2408a42f2bb102456817d064fefe
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-05-09 20:36:43 +0700

    pembaharuan ui

--- resources/views/all-post.blade.php
+++ resources/views/all-post.blade.php
@@ -1,59 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://www.cloudways.com/blog/wp-content/uploads/Laravel-9.jpg" class="d-block w-100 kustom_carousel" height="290" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Informasi terbaru soal teknologi</h5>
-        <p>semua kabar tentang teknologi terbaru</p>
-      </div>
+    <div class="tajuk_blog" style="height: 240px; width: 100%; background: teal; margin-top: -25px; padding: 60px">
+        <center>
+            <h1>Blog Laravel Bagus</h1>
+            <p>selamat datang di blog saya dengan konten menyegarkan</p>
+        </center>
     </div>
-    <div class="carousel-item">
-      <img src="https://www.cloudways.com/blog/wp-content/uploads/Laravel-9.jpg" class="d-block w-100 kustom_carousel" height="290" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Perkembangan pendidikan</h5>
-        <p>semua perubahan dan kebijakan baru sepeutar dunia pendidikan</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="https://www.cloudways.com/blog/wp-content/uploads/Laravel-9.jpg" class="d-block w-100 kustom_carousel" height="290" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Life and psychology</h5>
-        <p>semua saran serta tips and trick berkaitan dengan psikologi dan gaya hidup</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 
-@foreach ($posts as $item)
-	<div class="card" style="margin: 10px;">
-		<div class="card-body">
-			<h3 class="card-title">{{ $item->title }}</h3>
-      <span class="badge rounded-pill bg-success">{{ $item->kategori }}</span>
-			<p>{{ $item->penulis }}</p>
-			<h5>{{ $item->tanggal }}</h5>
-			<p class="card-text">{{ $item->excerpt }}</p>
-			<a href="/post/{{ $item->id }}">Lihat artikel...</a>
-		</div>
-	</div>
-@endforeach
+    <div class="promosi-dan-pencharian" style="width: 100%; background: #FF4949; height: 60px;">
+        <form class="d-flex" style="float: right; margin: 10px;">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
 
-</div>
+    @foreach ($posts as $item)
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+                <div class="card" style="margin: 10px;">
+                    <div class="card-body">
+                        <h3 class="card-title">{{ $item->title }}</h3>
+                        <span class="badge rounded-pill bg-success">{{ $item->kategori }}</span>
+                        <p>{{ $item->penulis }}</p>
+                        <h5>{{ $item->tanggal }}</h5>
+                        <p class="card-text">{{ $item->excerpt }}</p>
+                        <a href="/post/{{ $item->id }}">Lihat artikel...</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    </div>
 @endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -27,7 +27,6 @@
 @endforeach
 
 
-
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#tambahKomentar"
 style="margin: 15px; float: right;">
@@ -39,12 +38,10 @@ style="margin: 15px; float: right;">
 	<u><h5>Komentar</h5></u> 
 	<div>
 		<b>Nama Orang</b> |
-		<i>reaksi</i>
+		<i>email@gmail.com</i>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	</div>
 </div>
-
-
 
 <!-- Modal -->
 <div class="modal fade" id="tambahKomentar" tabindex="-1" aria-labelledby="tambahKomentar" aria-hidden="true">
