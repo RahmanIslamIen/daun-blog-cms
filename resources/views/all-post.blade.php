e25aa32413cafe40be851dce256defd95920782b
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-05-11 00:08:03 +0700

    penambahan footer

--- resources/views/all-post.blade.php
+++ resources/views/all-post.blade.php
@@ -32,33 +32,36 @@
         </div>
     @endforeach
 
-    <!-- Footer -->
-    <footer class="page-footer font-small bg-info pt-4">
+    <!-- styling global -->
+    <style>
+        a {
+            text-decoration: none;
+        }
 
+        li a {
+            color: white;
+        }
+
+    </style>
+
+    <!-- Footer -->
+    <footer class="page-footer font-small pt-4" style="background-color: #247881; margin-bottom: -24px">
         <!-- Footer Links -->
         <div class="container-fluid text-center text-md-left">
-
             <!-- Grid row -->
             <div class="row">
-
                 <!-- Grid column -->
                 <div class="col-md-6 mt-md-0 mt-3">
-
                     <!-- Content -->
                     <h5 class="text-uppercase">Footer Content</h5>
-                    <p>Here you can use rows and columns to organize your footer content.</p>
-
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, consectetur?</p>
                 </div>
                 <!-- Grid column -->
-
                 <hr class="clearfix w-100 d-md-none pb-3">
-
                 <!-- Grid column -->
                 <div class="col-md-3 mb-md-0 mb-3">
-
                     <!-- Links -->
-                    <h5 class="text-uppercase">Links</h5>
-
+                    <h5 class="text-uppercase">Bantuan</h5>
                     <ul class="list-unstyled">
                         <li>
                             <a href="#!">Link 1</a>
@@ -73,16 +76,13 @@
                             <a href="#!">Link 4</a>
                         </li>
                     </ul>
-
                 </div>
                 <!-- Grid column -->
 
                 <!-- Grid column -->
                 <div class="col-md-3 mb-md-0 mb-3">
-
                     <!-- Links -->
-                    <h5 class="text-uppercase">Links</h5>
-
+                    <h5 class="text-uppercase">Social Media</h5>
                     <ul class="list-unstyled">
                         <li>
                             <a href="#!">Link 1</a>
@@ -97,18 +97,15 @@
                             <a href="#!">Link 4</a>
                         </li>
                     </ul>
-
                 </div>
                 <!-- Grid column -->
-
             </div>
             <!-- Grid row -->
-
         </div>
-        <!-- Footer Links -->
 
+        <!-- Footer Links -->
         <!-- Copyright -->
-        <div class="footer-copyright text-center bg-info py-3">© 2020 Copyright:
+        <div class="footer-copyright text-center py-3" style="background-color: #005555;">© 2020 Copyright:
             <a href="/"> MDBootstrap.com</a>
         </div>
         <!-- Copyright -->
